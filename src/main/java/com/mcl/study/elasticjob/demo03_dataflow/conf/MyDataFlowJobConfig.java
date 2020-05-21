dc68768cb7303c8ec321455ec1804739fef195d6
蔡菲特 <785244892@qq.com>
2020-05-22 02:00:41 +0800

    新增 Dataflow demo

--- src/main/java/com/mcl/study/elasticjob/demo03_dataflow/conf/MyDataFlowJobConfig.java
+++ src/main/java/com/mcl/study/elasticjob/demo03_dataflow/conf/MyDataFlowJobConfig.java
@@ -1,16 +1,17 @@
-package com.mcl.study.elasticjob.demo02_boot.conf;
+package com.mcl.study.elasticjob.demo03_dataflow.conf;
 
+import com.dangdang.ddframe.job.api.dataflow.DataflowJob;
 import com.dangdang.ddframe.job.api.simple.SimpleJob;
 import com.dangdang.ddframe.job.config.JobCoreConfiguration;
+import com.dangdang.ddframe.job.config.dataflow.DataflowJobConfiguration;
 import com.dangdang.ddframe.job.config.simple.SimpleJobConfiguration;
-import com.dangdang.ddframe.job.lite.api.JobScheduler;
 import com.dangdang.ddframe.job.lite.config.LiteJobConfiguration;
 import com.dangdang.ddframe.job.lite.spring.api.SpringJobScheduler;
 import com.dangdang.ddframe.job.reg.base.CoordinatorRegistryCenter;
 import com.dangdang.ddframe.job.reg.zookeeper.ZookeeperConfiguration;
 import com.dangdang.ddframe.job.reg.zookeeper.ZookeeperRegistryCenter;
-import com.mcl.study.elasticjob.demo01.MyJob;
 import com.mcl.study.elasticjob.demo02_boot.MyBootJob;
+import com.mcl.study.elasticjob.demo03_dataflow.MyDataFlowJob;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
@@ -20,13 +21,13 @@ import org.springframework.context.annotation.Configuration;
  * @auth caiguowei
  * @date 2020/5/21
  */
-//@Configuration
-public class MyBootJobConfig {
+@Configuration
+public class MyDataFlowJobConfig {
 
     // zookeeper 地址
     private static final String zk_url = "112.74.169.206:2181";
     // 定时任务命名空间
-    private static final String job_namespace = "ej-example-java";
+    private static final String job_namespace = "ej-dataflow-java";
 
     /**
      * 1，配置注册中心
@@ -53,14 +54,14 @@ public class MyBootJobConfig {
      * @param shardingItemParameters
      * @return
      */
-    public LiteJobConfiguration liteJobConfiguration(Class<? extends SimpleJob> jobClass,String cron,int shadingTotalCount, String shardingItemParameters){
+    public LiteJobConfiguration liteJobConfiguration(Class<? extends DataflowJob> jobClass, String cron, int shadingTotalCount, String shardingItemParameters){
         // 创建JobCoreConfiguration(作业名称，cron表达式，分片数)
         JobCoreConfiguration.Builder builder = JobCoreConfiguration.newBuilder(jobClass.getName(), cron, shadingTotalCount);
         if (StringUtils.isNoneBlank(shardingItemParameters)){
             builder.shardingItemParameters(shardingItemParameters);
         }
-        // 创建SimpleJobConfiguration
-        SimpleJobConfiguration simpleJobConfiguration = new SimpleJobConfiguration(builder.build(), jobClass.getCanonicalName());
+        // DataflowJobConfiguration
+        DataflowJobConfiguration simpleJobConfiguration = new DataflowJobConfiguration(builder.build(), jobClass.getCanonicalName(),true);
         // 启动任务
         return LiteJobConfiguration
                 .newBuilder(simpleJobConfiguration)
@@ -71,9 +72,9 @@ public class MyBootJobConfig {
      * 3，创建SpringJobScheduler
      */
     @Autowired
-    private MyBootJob myBootJob;
+    private MyDataFlowJob myDataFlowJob;
     @Bean(initMethod = "init")
     public SpringJobScheduler springJobScheduler(CoordinatorRegistryCenter center){
-        return new SpringJobScheduler(myBootJob,center,liteJobConfiguration(myBootJob.getClass(),"0/3 * * * * ?",3,"0=a,1=b,2=c"));
+        return new SpringJobScheduler(myDataFlowJob,center,liteJobConfiguration(myDataFlowJob.getClass(),"0/3 * * * * ?",3,"0=a,1=b,2=c"));
     }
 }
